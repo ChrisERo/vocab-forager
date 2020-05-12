5a77fc4aa002d168c033856211a2ec146d535d83
cer95 <cer95@cornell.edu>
2020-05-11 20:54:11 -0400

    Got hilighting functionality without bolding.

--- get_hilights.js
+++ get_hilights.js
@@ -2,9 +2,17 @@ document.body.style.border = "5px solid red";
 
 function log_selection() {
     let selected = window.getSelection();
-    let range = selected.getRangeAt(0).getBoundingClientRect();
-    console.log(`SELECTED: ${selected.toString()}, ${range}`);
+    if (selected.toString() != '') {
+        let rangee = selected.getRangeAt(0);
+        console.log(`SELECTED: ${selected.toString()}, ${rangee}`);
+        
+	// after simulated retreival from non-volatile storage
+	let surrounding_node = document.createElement('div');
+        surrounding_node.setAttribute("id", "my_id");
+	surrounding_node.setAttribute('style', 'background-color: #ffff01; display: inline;')
+	rangee.surroundContents(surrounding_node);
+    }
 }
 
-document.onmouseup = log_selection
+document.onmouseup = log_selection;  // set action up
 
