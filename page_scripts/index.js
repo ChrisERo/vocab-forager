8a16a963ee1f724fe4a24c49a8772650fd33ebdf
cer95 <cer95@cornell.edu>
2020-06-13 18:40:39 -0400

    Added capability of removing site from storage entirely via a new webapge (added to index). Also fixed minor issues in edit_dicts.html (moving h2 element) and new_dicts: changed for value of a label.

--- page_scripts/index.js
+++ page_scripts/index.js
@@ -5,3 +5,7 @@ document.getElementById('new_dict').addEventListener("click", function () {
 document.getElementById('edit_dict').addEventListener("click", function () {
     window.location.href = 'edit_dict.html';
 });
+
+document.getElementById('see_sites').addEventListener("click", function () {
+    window.location.href = 'see_sites.html';
+});
