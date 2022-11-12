72d398a6caaf4f33437ab54d527dac5be2456b3a
ChrisERo <cer95@cornell.edu>
2022-11-11 20:08:36 -0500

    test: completed bg comm tests

--- src/utils/background-script-communication.test.ts
+++ src/utils/background-script-communication.test.ts
@@ -67,4 +67,124 @@ describe('Type Checks', () => {
     ])('Is %p a PageDataPiar', (object: any, isRequestType: boolean) => {
         expect(bgcom.isPageDataPair(object)).toEqual(isRequestType);
     });
+
+    it.each([
+        [{isActivated: true}, false],
+        [{url: 'https://www.google.com'}, true],
+        [{url: 'https://www.google.com', data: {wordEntries: [], missingWords: []}}, true],
+        [{url: 12345}, true],
+        [{url: 'https://www.test.net'}, true],
+        [null, false],
+        [undefined, false],
+    ])('Is %p a GetDataForPageRequest', (object: any, isRequestType: boolean) => {
+        expect(bgcom.isGetDataForPageRequest(object)).toEqual(isRequestType);
+    });
+
+    it.each([
+        [{isActivated: true}, false],
+        [
+            {
+                language: 'English', 
+                index: { language: 'English', index: 0}, 
+                content: {name: 'Canterbury', url: 'https://www.url.com'}
+            }, 
+            true
+        ],
+        [
+            {
+                language: 'Spanish', 
+                index: { language: 'Spanish', index: 37}, 
+                content: {name: 'SpanishDictionary', url: 'https://www.sp.es'}
+            }, 
+            true
+        ],
+        [
+            {
+                index: { language: 'Spanish', index: 37}, 
+                content: {name: 'SpanishDictionary', url: 'https://www.sp.es'}
+            }, 
+            false
+        ],
+        [
+            {
+                language: null,
+                index: { language: 'Spanish', index: 37}, 
+                content: {name: 'SpanishDictionary', url: 'https://www.sp.es'}
+            }, 
+            true
+        ],
+        [
+            {
+                language: null,
+                index: { index: 37 }, 
+                content: {name: 'SpanishDictionary', url: 'https://www.sp.es'}
+            }, 
+            false
+        ],
+        [
+            {
+                language: null,
+                index: { index: 37 }, 
+                content: { url: 'https://www.sp.es'}
+            }, 
+            false
+        ],
+        [null, false],
+        [undefined, false],
+    ])('Is %p a UpdateDictionaryRequest', (object: any, isRequestType: boolean) => {
+        expect(bgcom.isUpdateDictionaryRequest(object)).toEqual(isRequestType);
+    });
+
+    it.each([
+        [{isActivated: true}, false],
+        [{data: 'https://www.google.com'}, true],
+        [{data: 'https://www.google.com', url: {wordEntries: [], missingWords: []}}, true],
+        [{data: 12345}, true],
+        [{data: 'https://www.test.net'}, true],
+        [null, false],
+        [undefined, false],
+    ])('Is %p a LoadExtensionRequest', (object: any, isRequestType: boolean) => {
+        expect(bgcom.isLoadExtensionDataRequest(object)).toEqual(isRequestType);
+    });
+
+    it.each([
+        [{data: "todo"}, false],
+        [
+            {
+                messageType: bgcom.BSMessageType.LoadExtensionData,
+                payload: {data: "todo"}
+            }, 
+            true
+        ],
+        [
+            {
+                messageType: bgcom.BSMessageType.LoadExtensionData
+            }, 
+            false
+        ],
+        [
+            {
+                payload: {data: "todo"}
+            }, 
+            false
+        ],
+        [
+            {
+                messageType: bgcom.BSMessageType.AddNewDictionary,
+                payload: {lang: "Spanish", dict: {name: 'google', url: 'https://www.google.com'}}
+            }, 
+            true
+        ],
+        [
+            {
+                messageType: bgcom.BSMessageType.AddNewDictionary,
+                payload: {}
+            }, 
+            true
+        ],
+        [null, false],
+        [undefined, false],
+    ])('Is %p a BsMessage', (object: any, isRequestType: boolean) => {
+        expect(bgcom.isBsMessage(object)).toEqual(isRequestType);
+    });
 });
