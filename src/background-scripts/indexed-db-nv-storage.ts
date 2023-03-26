bbe0a03a53ba394e97baa14836d3ea0b844dcd7f
ChrisERo <cer95@cornell.edu>
2023-03-25 22:12:27 -0400

    feat: added more label query functions

--- src/background-scripts/indexed-db-nv-storage.ts
+++ src/background-scripts/indexed-db-nv-storage.ts
@@ -139,20 +139,17 @@ export class IndexedDBStorage implements NonVolatileBrowserStorage {
       * @param url url corresponding to siteData
       */
      storePageData(siteData: SiteData, url: string): Promise<void> {
+        const siteDataToStore = siteDataToIDBSiteData(siteData, url);
+        if (isEmpty(siteDataToStore)) {
+            return this.removePageData(url);
+        }
+        
         const query = (db: IDBDatabase): Promise<void> => {
             return new Promise((resolve, reject) => {
-                const siteDataToStore = siteDataToIDBSiteData(siteData, url);
                 const writeTransaction = db.transaction(IndexedDBStorage.SITE_DATA_TABLE, "readwrite");
                 const objectStore = writeTransaction.objectStore(IndexedDBStorage.SITE_DATA_TABLE);
     
-                let request: IDBRequest;
-                if (isEmpty(siteDataToStore)) {
-                    const schemePathSeparation: string[] = parseURL(url);
-                    request = objectStore.delete(schemePathSeparation);
-                }
-                else {
-                    request = objectStore.put(siteDataToStore, [siteDataToStore.schemeAndHost, siteDataToStore.urlPath]);
-                }
+                let request = objectStore.put(siteDataToStore, [siteDataToStore.schemeAndHost, siteDataToStore.urlPath]);
                 request.onerror = (err) => {
                     reject(`Unexpected error when saving ${url} ` + err);
                 };
@@ -162,6 +159,147 @@ export class IndexedDBStorage implements NonVolatileBrowserStorage {
             });
         };
 
+        return this.runQuery(query);
+    }
+
+    /**
+     * Adds entry in IndexedDB associating specified url with a label. If label is only 
+     * filled with empty spaces or is empty, this is a noop, but a console log is 
+     * recorded.
+     * 
+     * @param url URL of site
+     * @param label string representing a label
+     * @returns empty promise
+     */
+    addLabelEntry(url: string, label: string): Promise<void> {
+        label = label.trim();
+        const query = (db: IDBDatabase): Promise<void> => {
+            return new Promise((resolve, reject) => {
+                if (label.length === 0) {
+                    console.log(`Attempted to add empty subject to ${url}`);
+                    resolve();
+                }
+
+                const schemePathSeparation: string[] = parseURL(url);
+                const writeTransaction = db.transaction(IndexedDBStorage.LABEL_TABLE, "readwrite");
+                const objectStore = writeTransaction.objectStore(IndexedDBStorage.LABEL_TABLE);
+    
+                const request = objectStore.put('E', [label, schemePathSeparation[0], schemePathSeparation[1]]);
+                request.onerror = (err) => {
+                    reject(`Unexpected error when saving ${url} ` + err);
+                };
+                request.onsuccess = () => {
+                    resolve();
+                };
+            });
+        };
+        
+        return this.runQuery(query);
+    }
+
+    /**
+     * Removes association between a url and a label if one exists, otherwise, noop. Does
+     * nothing if label is empty (save for white spaces).
+     * 
+     * @param url URL of site
+     * @param label string representing a label
+     * @returns void promise
+     */
+    removeLabelEntry(url: string, label: string): Promise<void> {
+        label = label.trim();
+        const query = (db: IDBDatabase): Promise<void> => {
+            return new Promise((resolve, reject) => {
+                if (label.length === 0) {
+                    console.log(`Attempted to add empty subject to ${url}`);
+                    resolve();
+                }
+
+                const schemePathSeparation: string[] = parseURL(url);
+                const writeTransaction = db.transaction(IndexedDBStorage.LABEL_TABLE, "readwrite");
+                const objectStore = writeTransaction.objectStore(IndexedDBStorage.LABEL_TABLE);
+    
+                const request = objectStore.delete([label, schemePathSeparation[0], schemePathSeparation[1]]);
+                request.onerror = (err) => {
+                    reject(`Unexpected error when saving ${url} ` + err);
+                };
+                request.onsuccess = () => {
+                    resolve();
+                };
+            });
+        };
+        
+        return this.runQuery(query);
+    }
+
+    /**
+     * Compiles a list of all labels associated with specific url
+     * 
+     * @param url url for which to query for
+     * @returns list of all labels associated with specific url
+     */
+    getLabelsOfSpecificSite(url: string): Promise<string[]> {
+        const query = (db: IDBDatabase): Promise<string[]> => {
+            return new Promise((resolve, reject) => {
+                const schemePathSeparation: string[] = parseURL(url);
+                const writeTransaction = db.transaction(IndexedDBStorage.LABEL_TABLE, "readwrite");
+                const objectStore = writeTransaction.objectStore(IndexedDBStorage.LABEL_TABLE);
+                
+                const labelIndex = objectStore.index('url');
+                const request = labelIndex.openKeyCursor(IDBKeyRange.only(schemePathSeparation));
+                request.onerror = (err) => {
+                    reject(`Unexpected error when querying labels for ${url}: err `);
+                };
+                const results: string[] = [];
+                request.onsuccess = (event: any) => {
+                    let cursor: IDBCursor | null = event.target.result;
+                    if (cursor) {
+                        const key = cursor.primaryKey  as string[];
+                        results.push(key[0]);
+                        cursor.continue();
+                    } else {
+                        resolve(results);
+                    }
+                };
+            });
+        };
+        
+        return this.runQuery(query);
+    }
+    
+    /**
+     * Compiles a list of all URLs associated with specific label
+     * 
+     * @param label label for which to query for
+     * @returns list of all urls associated with specific label
+     */
+    getURLsOfSpecificLabels(label: string): Promise<string[]> {
+        const query = (db: IDBDatabase): Promise<string[]> => {
+            return new Promise((resolve, reject) => {
+                const writeTransaction = db.transaction(IndexedDBStorage.LABEL_TABLE, "readwrite");
+                const objectStore = writeTransaction.objectStore(IndexedDBStorage.LABEL_TABLE);
+                
+                const labelIndex = objectStore.index('label');
+                const request = labelIndex.openKeyCursor(IDBKeyRange.only(label));
+                request.onerror = (err) => {
+                    reject(`Unexpected error when querying urls for ${label}: ${err}`);
+                };
+                const results: string[] = [];
+                request.onsuccess = (event: any) => {
+                    let cursor: IDBCursor | null = event.target.result;
+                    if (cursor) {
+                        const key = cursor.primaryKey  as string[];
+                        const schemeAndHost = key[1];
+                        const path = key[2];
+                        const url = combineUrl(schemeAndHost, path);
+                        results.push(url);
+                        cursor.continue();
+                    } else {
+                        resolve(results);
+                    }
+                };
+            });
+        };
+        
         return this.runQuery(query);
     }
 
@@ -183,8 +321,8 @@ export class IndexedDBStorage implements NonVolatileBrowserStorage {
 
                 objectStoreSiteData.delete([schemeAndHost, urlPath]);
                 const labelEntriesCursor = labelIndex.openKeyCursor(IDBKeyRange.only(urlAsArray));
-                labelEntriesCursor.onsuccess = () => {
-                    let cursor = labelEntriesCursor.result;
+                labelEntriesCursor.onsuccess = (event: any) => {
+                    const cursor: IDBCursor | null = event.target.result;
                     if (cursor) {
                         objectStoreLabels.delete(cursor.primaryKey);
                         cursor.continue();
