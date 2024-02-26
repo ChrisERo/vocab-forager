8845a7b3a66acd5456783d2666055325d2e5ecc1
ChrisERo <cer95@cornell.edu>
2024-02-26 12:28:45 -0500

    test: added more contextmenu tests

--- src/background-scripts/contextmenu.test.ts
+++ src/background-scripts/contextmenu.test.ts
@@ -1,10 +1,13 @@
+import { CSMessageType } from "../utils/content-script-communication";
 import { ContextMenuManager } from "./contextmenu";
 import { MockDataStorage } from "./dictionary.test";
 import { MockLocalStorage } from "./indexed-db-nv-storage.test";
 
 
 const setUpBrowser = () => {
-    const contextMenuStuff: any = {}
+    const contextMenuStuff: any = {};
+    const messagesSent: any = {};
+    const listeners: ((info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab | undefined) => void) [] = [];
     global.chrome = {
         contextMenus: {
             contextMenuStuff: contextMenuStuff,
@@ -20,11 +23,21 @@ const setUpBrowser = () => {
                 return id;
             },
             onClicked: {
+                listeners: listeners,
                 addListener: (callback: (info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab | undefined) => void): void => {
-
+                    listeners.push(callback);
                 }
             }
+        },
+        tabs: {
+            messagesSent: messagesSent,
+            sendMessage: (tabId: number, message: any) => {
+                if (messagesSent[tabId] === undefined || messagesSent[tabId] === null) {
+                    messagesSent[tabId] = []
+                }
 
+                messagesSent[tabId].push(message);
+            }
         },
         storage: {
             local: new MockLocalStorage(),
@@ -91,6 +104,105 @@ describe('Contextmenu Tests', () => {
             expect(tabData.title).toBe(expectedTitle)
         }
 
+        return;
+    });
+
+    it.each([
+      [false, false],
+      [false, true],
+      [false, false],
+      [true, true],
+    ])('updateContextMenuBasedOnActivation %s %s', async (isActivatedOG: boolean, isActivatedNew: boolean) => {
+        setUpBrowser();
+        const globalDict = {languagesToResources: {}, currentDictionary: {index: -1, language: ''}};
+        const localStorage = new MockDataStorage(globalDict);
+        localStorage.setCurrentActivation(isActivatedOG);
+        const cmManager = new ContextMenuManager(localStorage);
+
+        await cmManager.setUpContextMenus();
+        const contextMenusStuff = (chrome.contextMenus as any)['contextMenuStuff']
+        if (isActivatedOG) {
+            expect(contextMenusStuff[ContextMenuManager.activationID].title).toBe(ContextMenuManager.deactivateActivationCMTitle)
+        } else {
+            expect(contextMenusStuff[ContextMenuManager.activationID].title).toBe(ContextMenuManager.activateActivationCMTitle)
+        }
+
+        cmManager.updateContextMenuBasedOnActivation(isActivatedNew);
+        if (isActivatedNew) {
+            expect(contextMenusStuff[ContextMenuManager.activationID].title).toBe(ContextMenuManager.deactivateActivationCMTitle)
+        } else {
+            expect(contextMenusStuff[ContextMenuManager.activationID].title).toBe(ContextMenuManager.activateActivationCMTitle)
+        }
+
+        return;
+    });
+
+    test("Delete Context Menu Hide and Show", async () => {
+        setUpBrowser();
+        const globalDict = {languagesToResources: {}, currentDictionary: {index: -1, language: ''}};
+        const localStorage = new MockDataStorage(globalDict);
+        localStorage.setCurrentActivation(true);
+        const cmManager = new ContextMenuManager(localStorage);
+
+        await cmManager.setUpContextMenus();
+        const contextMenusStuff = (chrome.contextMenus as any)['contextMenuStuff']
+        expect(contextMenusStuff[ContextMenuManager.deleteHighlightsID].visible).toBeFalsy();
+
+        cmManager.exposeDeleteContextMenu();
+        expect(contextMenusStuff[ContextMenuManager.deleteHighlightsID].visible).toBeTruthy();
+        cmManager.exposeDeleteContextMenu();
+        expect(contextMenusStuff[ContextMenuManager.deleteHighlightsID].visible).toBeTruthy();
+
+        cmManager.hideDeleteContextMenu();
+        expect(contextMenusStuff[ContextMenuManager.deleteHighlightsID].visible).toBeFalsy();
+        cmManager.hideDeleteContextMenu();
+        expect(contextMenusStuff[ContextMenuManager.deleteHighlightsID].visible).toBeFalsy();
+
+        cmManager.exposeDeleteContextMenu();
+        expect(contextMenusStuff[ContextMenuManager.deleteHighlightsID].visible).toBeTruthy();
+
+        return;
+    });
+
+
+
+
+    it.each([
+      [false, ContextMenuManager.quizID, null, false, null],
+      [false, ContextMenuManager.quizID, {id: 1}, true, {1: [{messageType: CSMessageType.StartQuiz}]}],
+      [false, ContextMenuManager.quizID, {id: 0}, true, {0: [{messageType: CSMessageType.StartQuiz}]}],
+      [false, ContextMenuManager.deleteHighlightsID, null, false, null],
+      [false, ContextMenuManager.deleteHighlightsID, {id: 1}, true, {1: [{messageType: CSMessageType.DeleteChosenHighlight}]}],
+      [false, ContextMenuManager.deleteHighlightsID, {id: 0}, true, {0: [{messageType: CSMessageType.DeleteChosenHighlight}]}],
+      [false, 'fakeCMId', null, false, null],
+      [false, 'fakeCMId', {id: 0}, false, null],
+
+    ])('Test CM Listener %s %s', async (isActivatedOG: boolean, menuItemId: string, tabInfo: any, shouldSucceede: boolean, messagesSentExpected: any) => {
+        setUpBrowser();
+        const globalDict = {languagesToResources: {}, currentDictionary: {index: -1, language: ''}};
+        const localStorage = new MockDataStorage(globalDict);
+        localStorage.setCurrentActivation(isActivatedOG);
+        const cmManager = new ContextMenuManager(localStorage);
+
+
+        await cmManager.setUpContextMenus();
+        const info = {menuItemId}
+
+        const listeners = (chrome.contextMenus.onClicked as any)['listeners'];
+        try {
+            listeners.forEach((element: any) => {
+                element(info, tabInfo);
+            });
+            expect(shouldSucceede).toBeTruthy();
+        } catch (ex) {
+            if (!shouldSucceede) {
+                return;
+            }
+            throw ex;
+        }
+
+        const sentMessages = (chrome.tabs as any)['messagesSent'];
+        expect(sentMessages).toEqual(messagesSentExpected);
         return;
     });
 });
