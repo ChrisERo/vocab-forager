0e77a6ccd846570b7acce6525030277a649881fd
ChrisERo <cer95@cornell.edu>
2024-10-02 20:07:09 -0400

    test: add tests from nodPathToNode

--- src/content-scripts/utils.test.ts
+++ src/content-scripts/utils.test.ts
@@ -1,5 +1,6 @@
 import { setUpMockBrowser } from "../background-scripts/mocks/chrome";
-import { defineWord } from "./utils";
+import { defineWord, nodPathToNode } from "./utils";
+import { JSDOM } from "jsdom";
 import { BSMessage, BSMessagePayload, BSMessageType, isBsMessage, isSearchRequest, SearchRequest } from "../utils/background-script-communication";
 
 describe('Contextmenu Tests', () => {
@@ -12,7 +13,7 @@ describe('Contextmenu Tests', () => {
             ['नमस्ते', true],
             ['good day', true],
             ['좋은 아침이에요', true],
-        ])('defineWord', (word: string|null, 
+        ])('defineWord %s %', (word: string|null, 
                           shouldHaveSent: boolean) => {
             setUpMockBrowser();
             const messageQueue: any[] = (chrome.runtime as any)
@@ -34,4 +35,88 @@ describe('Contextmenu Tests', () => {
                 expect(messageQueue).toHaveLength(originalQueueSize);
             }
         });
+
+    it.each([
+            [
+                '<body><p>Hello World</p></body>', 
+                [0],
+                'P',
+                'Hello World',
+            ],
+            [
+                '<body><p>Hello World</p></body>', 
+                [0,0],
+                '#text',
+                'Hello World',
+            ],
+            [
+                '<body><p>Hello World</p><p>Hola Mundo</p></body>', 
+                [1],
+                'P',
+                'Hola Mundo',
+            ],
+            [
+                '<body><p>Hello World</p><div><p>Hola Mundo</p></div></body>', 
+                [1],
+                'DIV',
+                'Hola Mundo',
+            ],
+            [
+                '<body><p>Hello World</p><div><p>Hola Mundo</p></div></body>', 
+                [1,0,0],
+                '#text',
+                'Hola Mundo',
+            ],
+            [
+                '<body>'+
+                    '<p>Hello World</p>' +
+                    '<div>' +
+                        '<p>Hola Mundo</p>' +
+                        '<p>¿Como Estas?</p>'+
+                        '<div>' +
+                            '<p>¿Como Estas?</p>' +
+                        '</div>'+
+                        '<p>Estoy bien. ¿Y tu?</p>'+
+                        '<ul>' +
+                            '<li>Bien</li>' +
+                        '</ul>'+
+                    '</div>' +
+                '</body>', 
+                [1,4,0],
+                'LI',
+                'Bien',
+            ],
+            [
+                '<body>'+
+                    '<p>Hello World</p>' +
+                    '<div>' +
+                        '<p>Hola Mundo</p>' +
+                        '<p>¿Como Estas?</p>'+
+                        '<div>' +
+                            '<p>¿Como Estas?</p>' +
+                        '</div>'+
+                        '<p>Estoy bien. ¿Y tu?</p>'+
+                        '<ul>' +
+                            '<li>Bien</li>' +
+                        '</ul>'+
+                    '</div>' +
+                '</body>', 
+                [1,4],
+                'UL',
+                'Bien',
+            ],
+        ])('nodePathToNode {%p} %s', (siteBody: string, nodePath: number[],
+                             expectedNodeName: string, expectedText: string) => {
+            jest.clearAllMocks();  // doing this beforeEach distorts setup test 
+            const pageContent: string = `<head></head>${siteBody}`;
+            const dom: JSDOM = new JSDOM(pageContent);
+            global.document = dom.window.document;
+
+            const result: Node = nodPathToNode(nodePath);
+            const nodeName = result.nodeName;
+            expect(nodeName).toBe(expectedNodeName);
+            const text = result.textContent;
+            expect(text).toBe(expectedText);
+        });
 });
+
